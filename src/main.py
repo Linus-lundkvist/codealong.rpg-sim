8464e2c9fbf8b13b73779bf4a76d4b44a5c58f3c
Linus.lundkvist <linus.em.lundkvist@gmail.com>
2022-02-11 12:56:42 +0100

    Added character class

---
+++ src/main.py
@@ -0,0 +1,25 @@
+from resources import Character
+
+
+def main():
+    mulle = Character("Mulle", 30, 28, 17)
+    bufa = Character("Bufa", 15, 48, 7)
+    adam = Character("Adam", 1, 0.5, -10)
+    eskil = Character("Eskil", 20, 20, 15)
+    råtta = Character("Råtta", 3, 2.5, 1)
+    salka = Character("salka", 10, 3, 25)
+
+
+
+    print(mulle)
+    print()
+    print(bufa)
+    print()
+    print(adam)
+    print()
+    print(råtta)
+    print()
+    print(eskil)
+
+if __name__ == "__main__":
+    main()
