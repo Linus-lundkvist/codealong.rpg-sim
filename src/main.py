9cc7d8652ff27f317fc624c60ba2223335173e45
Linus.lundkvist <linus.em.lundkvist@gmail.com>
2022-02-11 13:35:40 +0100

    Added enime and battel system

--- src/main.py
+++ src/main.py
@@ -1,25 +1,45 @@
-from resources import Character
+from re import S
+from resources import Character, Råtta
+import random 
+
+
+def fight(fighter : Character, enemies : list):
+
+    while not len(enemies) == 0:
+        fighter_target = random.choice(enemies)
+        fighter_target.take_damage(fighter.attack())
+        if fighter_target.get_health:
+            print("Target has died!")
+            enemies.remove(fighter_target)
+            if len(enemies) == 0: break
+    print(f"Fight is over! {fighter.name} won")
 
 
 def main():
+    enemies = []
+
     mulle = Character("Mulle", 30, 28, 17)
     bufa = Character("Bufa", 15, 48, 7)
-    adam = Character("Adam", 1, 0.5, -10)
     eskil = Character("Eskil", 20, 20, 15)
-    råtta = Character("Råtta", 3, 2.5, 1)
     salka = Character("salka", 10, 3, 25)
 
-
+    adam = Råtta("Adam", 1, 0.5, 0)
+    råtta = Råtta("Råtta", 3, 2.5, 1)
 
     print(mulle)
     print()
     print(bufa)
     print()
-    print(adam)
-    print()
-    print(råtta)
-    print()
     print(eskil)
+
+
+
+    enemies.append(adam)
+    enemies.append(råtta)
+    print("\nRåttor")
+    print(enemies[0],"\n",enemies[1])
+
+    fight(mulle, enemies)
 
 if __name__ == "__main__":
     main()
--- src/resources.py
+++ src/resources.py
@@ -12,8 +12,40 @@ class Character:
     
     def __str__(self) -> str:
         return f"Name: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+    
+    def take_damage(self, damage):
+        actual_damage = damage - self.armor
+        if (self.health -  actual_damage) < 0: self.health = 0
+        else: self.health -= actual_damage
 
+    def attack(self):
+        return self.damage
 
+    def get_health(self):
+        return self.health
+
+class Råtta:
+
+    def __init__(self, name, health, damage, armor) -> None:
+        self.name = name
+        self.health = health
+        self.damage = damage
+        self.armor = armor
+    
+    def __str__(self) -> str:
+        return f"ID: {self.name}\nHealth: {self.health}\nDamage: {self.damage}\nArmor: {self.armor}"
+
+    def take_damage(self, dmg):
+        actual_damage = dmg - self.armor
+        if (self.health -  actual_damage) < 0: self.health = 0
+        else: self.health -= actual_damage
+
+    def attack(self):
+        return self.damage
+    
+    def get_health(self):
+        return self.health
+    
 #functions
 def hello():
     print("Hello World")
